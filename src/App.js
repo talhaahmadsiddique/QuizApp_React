3fc0aaf3e9564844531f668a9669bd11e10efb66
talhaahmadsiddique <talhaahmadsiddique46@gmail.com>
2023-01-12 12:56:35 +0500

    food delivery change into excuse app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [excuse, setExcuse] = useState("");
 
-export default App
+  const fetchData = (excuse) => {
+    fetch(`https://excuser.herokuapp.com/v1/excuse/${excuse}/`)
+      .then((res) => res.json())
+      .then((data) => setExcuse(data[0].excuse));
+    // console.log(fetchData);
+  };
+  return (
+    <div className="h-screen w-screen flex justify-center py-20 bg-slate-400">
+      <div className="flex flex-col gap-5 items-center ">
+        <h1 className="text-red-200 text-6xl font-bold">Generate a excuse</h1>
+        <button
+          className="rounded-2xl bg-blue-200 w-fit px-5 py-2"
+          onClick={() => fetchData("family")}
+        >
+          family
+        </button>
+        <button
+          className="rounded-2xl bg-blue-200 w-fit px-5 py-2"
+          onClick={() => fetchData("office")}
+        >
+          office
+        </button>
+        <button
+          className="rounded-2xl bg-blue-200 w-fit px-5 py-2"
+          onClick={() => fetchData("party")}
+        >
+          party
+        </button>
+        <div>
+          <h1 className="text-red-200 text-6xl font-bold">{excuse}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
